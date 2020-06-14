a284d02a2d3c2561d6d748cc7277096dc6dbcacb
Matt Hayes <matt@mysterycommand.com>
2020-06-13 20:41:24 -0400

    feat(app): a 'device' with a kind of header/section/footer module layout

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,13 @@
 import React, { FC } from 'react';
+
 import style from './App.module.css';
 
-export const App: FC = () => <div className={style.App}>hello.</div>;
+export const App: FC = () => (
+  <div className={style.App}>
+    <article className={style.Device}>
+      <header className={style.Header}>Header</header>
+      <section className={style.Section}>Section</section>
+      <footer className={style.Footer}>Footer</footer>
+    </article>
+  </div>
+);
