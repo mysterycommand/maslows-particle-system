9bb4a9f5ae365355a96ad462b72cefbe09c8e73c
Matt Hayes <matt@mysterycommand.com>
2020-06-13 23:55:40 -0400

    test(app): get tests passing

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
 import { render } from '@testing-library/react';
+import React from 'react';
+
 import { Form } from './Form';
 
 test('renders Form', () => {
-  const { getByText } = render(<Form />);
-  const text = getByText(/Form/i);
+  const { getByText } = render(<Form dispatch={() => {}} />);
+  const text = getByText(/↑/i);
   expect(text).toBeInTheDocument();
 });
--- src/components/Message/Message.test.tsx
+++ src/components/Message/Message.test.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
 import { render } from '@testing-library/react';
+import React from 'react';
+
+import { Sender } from '../types';
+
 import { Message } from './Message';
 
+window.HTMLElement.prototype.scrollIntoView = () => {};
+
 test('renders Message', () => {
-  const { getByText } = render(<Message />);
+  const { getByText } = render(
+    <Message
+      createdAt={new Date().toISOString()}
+      sender={Sender.Self}
+      content={'Message'}
+    />,
+  );
   const text = getByText(/Message/i);
   expect(text).toBeInTheDocument();
 });
