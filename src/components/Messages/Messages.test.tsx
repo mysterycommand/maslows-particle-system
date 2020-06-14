fb835ebee18b987cf49ad37e28bd6201c5065c30
Matt Hayes <matt@mysterycommand.com>
2020-06-14 11:13:39 -0400

    test(app/messages): update tests post refactor

--- src/components/Messages/Messages.test.tsx
+++ src/components/Messages/Messages.test.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Messages } from './Messages';
+import { Sender } from '../types';
 
 test('renders Messages', () => {
-  const { getByText } = render(<Messages />);
-  const text = getByText(/Messages/i);
-  expect(text).toBeInTheDocument();
+  const { container } = render(
+    <Messages
+      messages={[
+        {
+          id: 'uuid',
+          createdAt: new Date().toISOString(),
+          sender: Sender.Self,
+          content: 'Messages',
+        },
+      ]}
+      messagesTop={0}
+      messagesHeight={665}
+      dispatch={() => {}}
+    />,
+  );
+  const ol = container.querySelector('ol');
+  expect(ol).toBeInTheDocument();
 });
