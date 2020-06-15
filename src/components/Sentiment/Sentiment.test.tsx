ee33fca11d0f98e36f2762d8c52890812aa65632
Matt Hayes <matt@mysterycommand.com>
2020-06-14 22:44:33 -0400

    test(app/sentiment): update tests

--- src/components/Sentiment/Sentiment.test.tsx
+++ src/components/Sentiment/Sentiment.test.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { Sentiment } from './Sentiment';
 
 test('renders Sentiment', () => {
-  const { getByText } = render(<Sentiment />);
-  const text = getByText(/Sentiment/i);
-  expect(text).toBeInTheDocument();
+  const { container } = render(<Sentiment dispatch={() => {}} />);
+  const div = container.querySelector('div');
+  expect(div).toBeInTheDocument();
 });
